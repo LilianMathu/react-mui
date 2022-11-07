a2c7e5e9c66d61414c4b2083921f0d0ff56f7eb9
Lilian Mathu <lilianmathu2018@gmail.com>
2022-11-08 00:46:37 +0300

    Typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import MuiTypography from "./Components/MuiTypography";
 
 function App() {
   return (
     <div className="App">
-      
+      <MuiTypography />
     </div>
   );
 }
